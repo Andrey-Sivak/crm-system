7eb9d4547000b408608b593c015b3d3e0fb26293
a.sivak <riziy_bes@mail.ru>
2021-03-05 11:36:27 +0300

    deploy

---
+++ precache-manifest.25b27c88c56a0859ed4b8880861b3fa8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4343e3e853428f86d7a9",
+    "url": "/crm-system/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "8c22e55e71b773bffc7a",
+    "url": "/crm-system/css/chunk-4e231633.36c39b93.css"
+  },
+  {
+    "revision": "0e2f1e4ca03d5f2bdfd3",
+    "url": "/crm-system/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "d279e22d948d325a232720fde780c4b8",
+    "url": "/crm-system/index.html"
+  },
+  {
+    "revision": "4343e3e853428f86d7a9",
+    "url": "/crm-system/js/app.149611ad.js"
+  },
+  {
+    "revision": "3577c6e65ba032164b38",
+    "url": "/crm-system/js/chunk-0d50b56f.88b4719a.js"
+  },
+  {
+    "revision": "d676801736babd84aea2",
+    "url": "/crm-system/js/chunk-1c52dd90.fd468070.js"
+  },
+  {
+    "revision": "7627f6967aeedb1e7eed",
+    "url": "/crm-system/js/chunk-1ec9c9e1.0e0e7f22.js"
+  },
+  {
+    "revision": "7a6ad4cea80edcc84fee",
+    "url": "/crm-system/js/chunk-2d0b377d.bf43bd2f.js"
+  },
+  {
+    "revision": "8c22e55e71b773bffc7a",
+    "url": "/crm-system/js/chunk-4e231633.1fbd5709.js"
+  },
+  {
+    "revision": "aeee56e7427920c474a0",
+    "url": "/crm-system/js/chunk-a273daa8.980f0a70.js"
+  },
+  {
+    "revision": "7c54c84882b1dee1f044",
+    "url": "/crm-system/js/chunk-d21e2c9a.48b2ba74.js"
+  },
+  {
+    "revision": "5bfe03dfcfb892ff8e04",
+    "url": "/crm-system/js/chunk-dc48058e.2679e71d.js"
+  },
+  {
+    "revision": "875e56aa1b8d192522ff",
+    "url": "/crm-system/js/chunk-e5ad4084.0f59ac90.js"
+  },
+  {
+    "revision": "0e2f1e4ca03d5f2bdfd3",
+    "url": "/crm-system/js/chunk-vendors.cc32d23e.js"
+  },
+  {
+    "revision": "bbffb38cb0cf9c117c93257fffc9ff4d",
+    "url": "/crm-system/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-system/robots.txt"
+  }
+]);
